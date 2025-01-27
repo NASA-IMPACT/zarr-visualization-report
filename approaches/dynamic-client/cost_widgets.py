4a683619da4d030349288d6d35019ed4c550bee4
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-01-27 10:40:05 -0500

    Lint

--- approaches/dynamic-client/cost_widgets.py
+++ approaches/dynamic-client/cost_widgets.py
@@ -1,3 +1,5 @@
+"""Utilities for estimating storage costs of pyramids."""
+
 import numpy as np
 import panel as pn
 
@@ -5,6 +7,22 @@ import panel as pn
 def calculate_level_size(*, level, pixels_per_tile, extra_dimension_length, data_dtype):
     """
     Calculate the uncompressed size for a given zoom level in GB.
+
+    Parameters
+    ----------
+    level : int
+        Zoom level.
+    pixels_per_tile : int
+        Scale of each tile (commonly 128, 256, or 512).
+    extra_dimension_length : int
+        Length of the non-spatial dimension.
+    data_dtype : np.dtype
+        Numpy dtype, expected to contain an .itemsize property.
+
+    Returns
+    -------
+    int
+        Uncompressed size of the given zoom level in GB.
     """
     data_size = (
         (pixels_per_tile * 2**level) ** 2
@@ -12,9 +30,7 @@ def calculate_level_size(*, level, pixels_per_tile, extra_dimension_length, data
         * data_dtype.itemsize
         * 1e-9
     )
-    spatial_coords_size = (
-        (pixels_per_tile * 2**level * 2) * data_dtype.itemsize * 1e-9
-    )
+    spatial_coords_size = (pixels_per_tile * 2**level * 2) * data_dtype.itemsize * 1e-9
     extra_coords_size = extra_dimension_length * data_dtype.itemsize * 1e-9
     return data_size + spatial_coords_size + extra_coords_size
 
@@ -29,8 +45,27 @@ def calculate_pyramid_cost(
     price_per_GB,
 ):
     """
-    Calculated the uncompressed size for pyramids with a given number of zoom levels
-    in GB.
+    Calculated the expected cost of storing the overviews.
+
+    Parameters
+    ----------
+    number_of_zoom_levels : int
+        Number of zoom levels for the specific dataset.
+    pixels_per_tile : int
+        Scale of each tile (commonly 128, 256, or 512).
+    extra_dimension_length : int
+        Length of the non-spatial dimension.
+    data_dtype : np.dtype
+        Numpy dtype, expected to contain an .itemsize property.
+    data_compression_ratio : float
+        Expected ratio between compressed and uncompressed data
+    price_per_GB : float
+        Expected storage cost per GB
+
+    Returns
+    -------
+    float
+        Expected cost of storing the overviews in $.
     """
     data_dtype = np.dtype(data_dtype)
     pyramid_size = 0
